20b672c8ebc1266296686b5e38ea2c62ffa8ded9
Connor McGraw <42752783+Conmcgr@users.noreply.github.com>
2025-02-02 22:41:18 +0000

    functional but not the style they want

--- app.rb
+++ app.rb
@@ -1,9 +1,47 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
+require 'dotenv'
+
+Dotenv.load
+
+key = ENV["EXCHANGE_RATE_KEY"]
+
+exchange_url = "https://api.exchangerate.host/list?access_key=" + key
+
+raw_response = HTTP.get(exchange_url)
+
+parsed_response = JSON.parse(raw_response)
+
+currencies_list = parsed_response.fetch("currencies").keys
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  @currencies = currencies_list
+
+  erb(:homepage)
+end
+
+get("/convert") do
+
+  @current = params.fetch("current")
+
+  @currencies = currencies_list
+
+  erb(:convert)
+end
+
+get("/conversion") do
+  @from = params.fetch("from")
+  @to = params.fetch("to")
+  url = "https://api.exchangerate.host/convert?from=" + @from + "&to=" + @to + "&amount=1&access_key=" + key
+
+  raw_conversion = HTTP.get(url)
+
+  parsed_conversion = JSON.parse(raw_conversion)
+
+  @amount = parsed_conversion["result"]
+
+  erb(:conversion)
 end
