ba652242f5ecc0fe82457d1c0a91ce9067207bc1
Connor McGraw <42752783+Conmcgr@users.noreply.github.com>
2025-02-03 02:59:54 +0000

    finished

--- app.rb
+++ app.rb
@@ -8,40 +8,48 @@ Dotenv.load
 
 key = ENV["EXCHANGE_RATE_KEY"]
 
-exchange_url = "https://api.exchangerate.host/list?access_key=" + key
-
-raw_response = HTTP.get(exchange_url)
-
-parsed_response = JSON.parse(raw_response)
-
-currencies_list = parsed_response.fetch("currencies").keys
 
 get("/") do
+
+  exchange_url = "https://api.exchangerate.host/list?access_key=" + key
+
+  raw_response = HTTP.get(exchange_url)
+
+  parsed_response = JSON.parse(raw_response)
+
+  currencies_list = parsed_response.fetch("currencies").keys
 
   @currencies = currencies_list
 
   erb(:homepage)
 end
 
-get("/convert") do
+get "/:from/:to" do
+  @from = params[:from]
+  @to = params[:to]
+  
+  conversion_url = "https://api.exchangerate.host/convert?from=#{@from}&to=#{@to}&amount=1&access_key=#{key}"
+  raw_conversion = HTTP.get(conversion_url)
+  parsed_conversion = JSON.parse(raw_conversion)
+  @amount = parsed_conversion["result"]
 
-  @current = params.fetch("current")
+  erb :conversion
+end
+
+get "/:current" do
+
+  exchange_url = "https://api.exchangerate.host/list?access_key=" + key
+
+  raw_response = HTTP.get(exchange_url)
+
+  parsed_response = JSON.parse(raw_response)
+
+  currencies_list = parsed_response.fetch("currencies").keys
 
   @currencies = currencies_list
 
-  erb(:convert)
-end
-
-get("/conversion") do
-  @from = params.fetch("from")
-  @to = params.fetch("to")
-  url = "https://api.exchangerate.host/convert?from=" + @from + "&to=" + @to + "&amount=1&access_key=" + key
-
-  raw_conversion = HTTP.get(url)
-
-  parsed_conversion = JSON.parse(raw_conversion)
-
-  @amount = parsed_conversion["result"]
-
-  erb(:conversion)
+  @current = params[:current]
+  @currencies = currencies_list
+  
+  erb :convert
 end
